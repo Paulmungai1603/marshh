373c28817e086ac700d02d75ac84a1473aae6ed9
marshmellow0007 <97960418+marshmellow0007@users.noreply.github.com>
2022-02-15 17:13:17 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,30 +1,42 @@
 /*
- county library management system
- by marshmellow
- feb 2022
- MIT License
- C89 compiler
+    simple calculator
+    by Marshmellow
+    last updated on 15th feb 2022
+    C89 Compiler
+MIT license
 */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int menu() {
-   int action;
-   printf("select an action below\n");
-   printf("1. Add new patron\n");
-   printf("2. View patrons\n");
-   printf("3. View Books\n");
-   printf("4. Add New Book\n");
-   printf("Your Action: ");
-   scanf("%d",&action);
-   return action;
-
-}
 int main()
-
 {
-    printf("COUNTY LIBRARY SYSTEM \n");
-    printf("Welcome marshmellow \n");
-    printf("you selected action %d",menu());
+    //variable declaration
+    char name[100];
+    int n1,n2,sum,product,diff;
+    float quot;
+    printf("Simple calculator!\n");
+
+    // input
+    printf("Enter your name:");
+    gets(name);
+    printf("Enter two integers: ");
+    scanf("%d%d",&n1,&n2);
+
+    //computations
+    sum= n1 + n2;
+    diff = n1 - n2;
+    product =n1 * n2;
+     quot = (float) n1/n2;
+
+    //output
+     printf("Hey %s, here are the results: \n",name);
+     printf("%d+%d=%d\n",n1,n2,sum);
+     printf("%d-%d=%d\n",n1,n2,diff);
+     printf("%d*%d=%d\n",n1,n2,product);
+     printf("%d/%d=%f\n",n1,n2,quot);
+
+
     return 0;
 }
